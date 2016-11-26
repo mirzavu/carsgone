f55d0af2a3ec725cf8120168ca1f04714b5fc7de
Mirza <mirza.ekm@gmail.com>
2016-11-26 13:08:31 +0530

    zindex issue in post page

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'mirza.ekm@gmail.com', 'name' => "Mirza V U"],
 
     /*
     |--------------------------------------------------------------------------

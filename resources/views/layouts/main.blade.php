4515a603fca257376dadd42f266f209f077c12d6
Mirza V U <mirza.ekm@gmail.com>
2017-03-11 23:25:06 +0530

    google analytics tracking code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,6 +27,21 @@
     <![endif]-->
 </head>
 <body class="home">
+<script>
+  console.log(document.location.hostname)
+  if(document.location.hostname.search("carsgone.com") !== -1)
+  {
+    console.log('cc')
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-6617033-1', 'auto');
+    ga('send', 'pageview');
+  }
+  
+</script>
 <!-- header start -->
 <header>
 	<!-- header upper start -->

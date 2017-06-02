72b71f8d34e3b3045532274da2367eb9c4893e29
Mirza V U <mirza.ekm@gmail.com>
2017-06-02 08:25:44 +0530

    User posting as private

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -103,10 +103,10 @@ class PostController extends Controller
 		//Change user's lat, long, city, prov from postal code
 		$user->phone = $request['phone'];
 		$user->postal_code = $request['postal_code'];
-		if($request->role == "dealer")
-			$user->role = "dealer";
-		else
-			$user->role = "member";
+		// if($request->role == "dealer")
+		// 	$user->role = "dealer";
+		// else
+		$user->role = "member";
 		$url = "http://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($user->postal_code);
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);

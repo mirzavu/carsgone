3a9d2b6ce88b513e62c3d38a36797279ac0a8d2a
Mirza V U <mirza.ekm@gmail.com>
2017-09-01 17:24:50 +0530

    Boost body style fix

--- app/Console/Commands/Boost.php
+++ app/Console/Commands/Boost.php
@@ -165,7 +165,11 @@ class Boost extends Command
                 
                 $vehicle->model_id = $model->id;
 
-                $body_style_id = BodyStyleGroup::where('body_style_group_name',(string) $xml->Body_Style)->value('id');
+                
+                $body_style = (string) $xml->BodyStyle;
+                $body_style = $body_style == "Pickup Truck"? "Truck" : $body_style;
+
+                $body_style_id = BodyStyleGroup::where('body_style_group_name',$body_style)->value('id');
                 if($body_style_id)
                     $vehicle->body_style_group_id = $body_style_id;
                 else

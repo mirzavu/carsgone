b7cc471c6465a75ba1d64c8421a2f13d6d27e59a
Mirza V U <mirza.ekm@gmail.com>
2017-09-12 12:51:03 +0530

    EAL link

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -159,6 +159,7 @@
                                 <li><a href="/help">Questions &amp; Answers</a></li>
                                 <li><a href="/privacy">Privacy Policy</a></li>
                                 <li><a href="/contact">Contact Us</a></li>
+                                <li><a href="https://www.edmontonautoloans.com">Link</a></li>
                             </ul> 
                          </div>
                     </div>

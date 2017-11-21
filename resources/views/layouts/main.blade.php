d815e7d0ec5e09448667a7f6f4aa6c70f47c5e0c
Mirza V U <mirza.ekm@gmail.com>
2017-11-21 22:32:53 +0530

    FB pixel code for canadadrives

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -37,9 +37,26 @@
 
     ga('create', 'UA-6617033-1', 'auto');
     ga('send', 'pageview');
+
+    /* -- Facebook Pixel Code -- */
+
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '512056242245615');
+    fbq('track', 'PageView');
+
+    /* -- End Facebook Pixel Code -- */
   } 
-  
 </script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=512056242245615&ev=PageView&noscript=1"
+/></noscript>
 <!-- header start -->
 <header>
 	<!-- header upper start -->
